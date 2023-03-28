08c66ae51fb4a7c8c25a0d2113636af3bd5ca2d9
pl3lee <billylee123@gmail.com>
2023-03-28 11:29:54 -0400

    home page

--- client/src/pages/create-recipe.tsx
+++ client/src/pages/create-recipe.tsx
@@ -1,13 +1,99 @@
+import { Box, Button, FormControl, Input, InputLabel, TextareaAutosize, Typography } from '@mui/material';
+import { title } from 'process';
 import * as React from 'react';
+import { useState } from 'react';
+import { useGetUserID } from '@/hooks/useGetUserID';
+import { useRouter } from 'next/router';
 
 export interface CreateRecipeProps {
 }
 
+export interface Recipe {
+  name: string;
+  ingredients: string[];
+  instructions: string;
+  imageURL: string;
+  cookingTime: number;
+  userOwner: any;
+}
+
 const CreateRecipe = (props: CreateRecipeProps) => {
+  const router = useRouter();
+  const userID = useGetUserID();
+  const [recipe, setRecipe] = useState<Recipe>({
+    name: '',
+    ingredients: [],
+    instructions: "",
+    imageURL: "",
+    cookingTime: 0,
+    userOwner: userID,
+  });
+  
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement|HTMLTextAreaElement>) => {
+    // this automatically gets the name property from the input, and the value
+    const { name, value } = event.target;
+    // we are only changing the property that was changed
+    setRecipe({...recipe, [name]: value})
+  }
+
+  const handleIngredientChange = (event: React.ChangeEvent<HTMLInputElement|HTMLTextAreaElement>, index: number) => {
+    const { value } = event.target;
+    const ingredients = [...recipe.ingredients];
+    ingredients[index] = value;
+    setRecipe({...recipe, ingredients})
+  }
+  const addIngredient = () => {
+    setRecipe({...recipe, ingredients: [...recipe.ingredients, '']})
+  }
+  const onSubmit = async (event: any) => {
+    event.preventDefault();
+    try {
+      const res = await fetch('http://localhost:3001/recipes', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify(recipe),
+    });
+    alert("recipe created")
+    router.push("/");
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
-    <div>
-      CreateRecipe PAGE
-    </div>
+    <>
+    <Typography variant='h2'>Create Recipe</Typography>
+    <Box component="form" autoComplete="off" onSubmit={onSubmit}>
+      <>
+      {/* we put the name property here so we can use it in handleChange */}
+      <FormControl variant="standard">
+        <InputLabel htmlFor="name">Name</InputLabel>
+        <Input id="name" type="text" name="name" required onChange={handleChange}/>
+      </FormControl>
+      <FormControl variant="standard">
+        <Button onClick={addIngredient}>Add Ingredient</Button>
+        {recipe.ingredients.map((ingredient, index) => {
+          return (
+          <Input key={index} type="text" name="ingredients" value={ingredient} onChange={(event) => handleIngredientChange(event, index)}/>)
+        })}
+      </FormControl>
+      <FormControl variant="standard">
+        <InputLabel htmlFor="instructions">Instructions</InputLabel>
+        <TextareaAutosize id="instructions" name="instructions" required placeholder='Instructions' onChange={handleChange}/>
+      </FormControl>
+      <FormControl variant="standard">
+        <InputLabel htmlFor="imageURL">Image URL</InputLabel>
+        <Input id="imageURL" type="text" name="imageURL" required onChange={handleChange}/>
+      </FormControl>
+      <FormControl variant="standard">
+        <InputLabel htmlFor="cookingTime">Cooking time</InputLabel>
+        <Input id="cookingTime" type="number" name="cookingTime" required onChange={handleChange}/>
+      </FormControl>
+      <Button type="submit" variant="contained" color="primary">
+        Submit
+      </Button>
+      </>
+    </Box>
+    </>
   );
 }
 
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,14 +3,82 @@ import { Inter } from '@next/font/google'
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '@/components/Layout'
+import { useState } from 'react'
+import { GetServerSideProps } from 'next'
+import { Recipe } from './create-recipe'
+import { Button, Typography } from '@mui/material'
+import { useGetUserID } from '@/hooks/useGetUserID'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const Home = () => {
+interface HomeProps {
+  recipes: any;
+}
+
+const Home: React.FC<HomeProps> = ({recipes}) => {
+  const userID = useGetUserID();
+  // console.log(savedRecipes)
+  const saveRecipe = async (recipeID: any) => {
+    try {
+      const res = await fetch('http://localhost:3001/recipes', {
+      method: 'PUT',
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify({recipeID, userID}),
+    });
+    alert("recipe saved")
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
-    <>This is the home page</>
+    <>
+    <Typography variant="h1">Recipes</Typography>
+    <ul>
+    {recipes.map((recipe: any) => (<li key={recipe._id}>
+      <div>
+        <Typography variant="h2">{recipe.name}</Typography>
+      </div>
+      <Button onClick={() => saveRecipe(recipe._id)}>Save</Button>
+      <div>
+        <Typography variant="h3">Instructions</Typography>
+        <Typography variant='body1'>{recipe.instructions}</Typography>
+      </div>
+      <div>
+        <Typography variant="h3">Image</Typography>
+        <img src={recipe.imageURL} alt={recipe.name}/>
+      </div>
+      <div>
+        <Typography variant="h3">Cooking Time (In minutes)</Typography>
+        <Typography variant='body1'>{recipe.cookingTime}</Typography>
+      </div>
+    </li>))}
+    </ul>
+    </>
       
   )
+}
+
+export const getServerSideProps: any = async () =>{
+  try {
+  const res = await fetch('http://localhost:3001/recipes', {
+    method: 'GET',
+  });
+  const recipes = await res.json();
+  // const savedRecipesData = await fetch('http://localhost:3001/recipes/savedRecipes/id', {
+  //   method: 'GET'
+  // });
+  // const savedRecipes = await savedRecipesData.json();
+  if (recipes) {
+    return {
+      props: { recipes },
+    }
+  }
+  } catch (err) {
+    console.log(err);
+    return {
+      props: {recipes: []},
+    }
+  }
 }
 
 export default Home
